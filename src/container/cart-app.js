a6ec7efe2d3c1a401b63e4c76721dc2c8a5d8491
Mengting-Lin <s400110605@gmail.com>
2020-06-19 01:08:43 +0800

    0619-掛入cart-checkOrder.js
    1. 增加cart-checkOrder.js檔案
    2. cart-app.js增加路由

--- src/container/cart-app.js
+++ src/container/cart-app.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import CartCheckout from '../components/cart/cart-checkout';
+import CartCheckOrder from '../components/cart/cart-checkOrder';
 import Navbar from '../components/navbar/navbar';
 import Footer from '../components/footer/footer';
 
@@ -10,6 +11,7 @@ function CartApp(){
         <>
             {/* <Navbar /> */}
             <CartCheckout />
+            <CartCheckOrder />
             {/* <Footer />         */}
         </>
     )
