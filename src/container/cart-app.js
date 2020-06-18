1f491d3ac52239d50961a5560f4aee3b05965392
Mengting-Lin <s400110605@gmail.com>
2020-06-18 15:36:06 +0800

    0618-把我的東西掛進去

---
+++ src/container/cart-app.js
@@ -0,0 +1,18 @@
+import React,{useState,useEffect} from 'react';
+import Cart from '../components/cart/cart';
+import Navbar from '../components/navbar/navbar';
+import Footer from '../components/footer/footer';
+
+function CartApp(){
+
+
+    return(
+        <>
+            <Navbar />
+            <Cart />
+            <Footer />        
+        </>
+    )
+}
+
+export default CartApp;
