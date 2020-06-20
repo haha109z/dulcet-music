1f9076c75f831b0ba47d3ed77f4d2f96d7ef4bb3
陳哲宇 <milk840118@yahoo.com.tw>
2020-06-20 15:49:27 +0800

    0620 15:49 AnyQuestionAction RWD End

--- src/components/AnyQuestion/AnyQuestionList/AnyQuestionAction.js
+++ src/components/AnyQuestion/AnyQuestionList/AnyQuestionAction.js
@@ -1,48 +1,47 @@
 import React from 'react'
 // import '../../../../styles/custom.scss'
 function AnyQuestionAction() {
-  return <><div className="AnyQuestionActionContainer">
+  return <>
+<div className="AnyQuestionActionAll">
+<div className="AnyQuestionActionContainer">
     <div className="Text123"></div>
-    <div style={{display:'flex'}}>
+    <div className="AnyQuestionActionFlex">
     <div className="AnyQuestionActionCard AnyQuestionActionCardLeft">
             <i className="far fa-user AnyQuestionActionCardIcon"></i>
             <h2 className="AnyQuestionActionCardIconH2">會員問題</h2>
           </div>
-          <table class="AnyQuestionActionTable">
-            <thead>
-              <tr>
-                <td style={{display:'flex'}}>
+          <div class="AnyQuestionActionTable AnyQuestionActionRwd">
+            <div style={{display:'flex'}}>
                 <input type="submit" value="忘記帳號"/>
                 <i class="AnyQuestionActionIcon fas fa-check"></i>
-                </td>
-              </tr>
-              <tr>
-                <td style={{display:'flex'}}><input type="submit" value="忘記密碼"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-              <tr >
-                <td style={{display:'flex'}}><input type="submit" value="帳號綁定"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-              <tr>
-                <td style={{display:'flex'}}><input type="submit" value="帳號問題"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-              <tr>
-                <td style={{display:'flex'}}><input type="submit" value="信箱認證"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-              <tr>
-                <td style={{display:'flex'}}><input type="submit" value="優惠卷使用方法"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-              <tr>
-                <td style={{display:'flex'}}><input type="submit" value="帳號遺失密碼怎麼辦"/>
-                <i class="AnyQuestionActionIcon fas fa-check"></i></td>
-              </tr>
-            </thead>
-          </table>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="忘記密碼"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="帳號綁定"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="帳號問題"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="信箱認證"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="優惠卷使用方法"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
+                <div style={{display:'flex'}}>
+                <input type="submit" value="帳號遺失密碼怎麼辦"/>
+                <i class="AnyQuestionActionIcon fas fa-check"></i>
+                </div>
           </div>
+          </div>
+  </div>
   </div></>
 }
 
