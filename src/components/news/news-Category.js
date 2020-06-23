259e120009a0b3ff9b745266f83640e6fcbd8c3e
haha109z <haha821009@gmail.com>
2020-06-23 16:09:47 +0800

    0623-news修改分類方式

--- src/components/news/news-Category.js
+++ src/components/news/news-Category.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import Button from 'react-bootstrap/Button';
 
-import { DropdownButton } from 'react-bootstrap';
-import { Dropdown } from 'react-bootstrap';
-import { ButtonGroup } from 'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
 
-
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 150,
+  },
+}))
 
 function NewsCategory(props) {
+  const classes = useStyles()
   return (
     <>
       <div className="news-CategoryContainer d-flex news-NotoSerifTC">
@@ -28,57 +37,19 @@ function NewsCategory(props) {
           </ul>
         </div>
 
-        <div className="news-Dropdown news-NotoSerifTC news-DropdownNews">
-          {['News類型'].map(
-            (variant) => (
-              <>
-                <DropdownButton
-                  as={ButtonGroup}
-                  key={variant}
-                  id={`dropdown-variants-${variant}`}
-                  variant={variant.toLowerCase()}
-                  title={variant}
-                >
-                  <Dropdown.Item  eventKey="" active>全部</Dropdown.Item>
-                  <Dropdown.Item eventKey="P">公告</Dropdown.Item>
-                  <Dropdown.Item eventKey="C">課程</Dropdown.Item>
-                  <Dropdown.Item eventKey="A">活動</Dropdown.Item>
-                </DropdownButton>{' '}
-              </>
-            )
-          )}
-        </div>
-
-        <div className="news-Dropdown news-NotoSerifTC">
-          {/* <button type="button" className="news-DropdownBtn">
-            月份篩選&nbsp;&nbsp;<i class="fas fa-sort"></i>
-          </button> */}
-          {['月份篩選'].map(
-            (variant) => (
-              <>
-                <DropdownButton
-                  as={ButtonGroup}
-                  key={variant}
-                  id={`dropdown-variants-${variant}`}
-                  variant={variant.toLowerCase()}
-                  title={variant}
-                >
-                  <Dropdown.Item  eventKey="Jan" active>一月 ( January )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Feb">二月 ( February )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Mar">三月 ( March )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Apr">四月 ( April )</Dropdown.Item>
-                  <Dropdown.Item eventKey="May">五月 ( May )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Jun">六月 ( June )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Jul">七月 ( July )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Aug">八月 ( August )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Sep">九月 ( September )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Oct">十月 ( October )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Nov">十一月 ( November )</Dropdown.Item>
-                  <Dropdown.Item eventKey="Dec">十二月 ( December )</Dropdown.Item>
-                </DropdownButton>{' '}
-              </>
-            )
-          )}
+        <div className="news-Month news-NotoSerifTC">
+          <form className={classes.container} noValidate>
+            <TextField
+              id="date"
+              label="月份篩選"
+              type="month"
+              defaultValue="date"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </form>
         </div>
       </div>
     </>
