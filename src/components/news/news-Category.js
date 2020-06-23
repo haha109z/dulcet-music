d569a224e87fca62b08b4faf842abf4e8dc42b3e
haha109z <haha821009@gmail.com>
2020-06-23 16:43:24 +0800

    0623-新增一個套件@material-ui

--- src/components/news/news-Category.js
+++ src/components/news/news-Category.js
@@ -1,55 +1,73 @@
 import React, { useState, useEffect } from 'react'
 
-import { makeStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 150,
-  },
-}))
+  formControl: {
+    minWidth: 120,
+  }
+}));
+
 
 function NewsCategory(props) {
-  const classes = useStyles()
+
+  const classes = useStyles();
+  const [state, setState] = useState({
+    month: '',
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+  
   return (
     <>
       <div className="news-CategoryContainer d-flex news-NotoSerifTC">
         <div className="news-Category">
           <ul className="d-flex justify-content-start">
             <li>
-              <a href="#">全部</a>
+              <a href="">全部</a>
             </li>
             <li>
-              <a href="#">公告</a>
+              <a href="">公告</a>
             </li>
             <li>
-              <a href="#">課程</a>
+              <a href="">課程</a>
             </li>
             <li>
-              <a href="#">活動</a>
+              <a href="">活動</a>
             </li>
           </ul>
         </div>
 
         <div className="news-Month news-NotoSerifTC">
-          <form className={classes.container} noValidate>
-            <TextField
-              id="date"
-              label="月份篩選"
-              type="month"
-              defaultValue="date"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </form>
+          
+        <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel htmlFor="outlined-age-native-simple">月份篩選</InputLabel>
+        <Select
+          native
+          value={state.age}
+          onChange={handleChange}
+          label="Age"
+          inputProps={{
+            name: 'age',
+            id: 'outlined-age-native-simple',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value={10}>Ten</option>
+          <option value={20}>Twenty</option>
+          <option value={30}>Thirty</option>
+        </Select>
+      </FormControl>
+
         </div>
       </div>
     </>
