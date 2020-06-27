6b9590a1ee29f5cac5c28e69643f3e8ce810a596
haha109z <haha821009@gmail.com>
2020-06-27 13:37:13 +0800

    0627-新增dulcet_music.sql,修改db_connenct2裡面的資料庫名稱

--- Nodejs/server/src/db_connect2.js
+++ Nodejs/server/src/db_connect2.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host:"localhost",
     user:"root",
     password:"root",
-    database:"coupon",
+    database:"dulcet_music",
     waitForConnections:true,
     connectionLimit: 10,
     queueLimit: 0,
