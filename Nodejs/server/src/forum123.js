2f2dbf50f91860dc2dcbcc589e1bde71e00e0651
陳哲宇 <milk840118@yahoo.com.tw>
2020-06-29 19:38:37 +0800

    0629 新增檔案

--- Nodejs/server/src/forum123.js
+++ Nodejs/server/src/forum123.js
@@ -11,9 +11,9 @@ router.post('/', async (req, res) => {
   let {
     ForumTitle,
     ForumAction,
-    ForumActionAns,
     userID
   } = req.body;
+  
 
   // let {ForumId} = req.body;
   // let resData = { code: '', msg: '' };
@@ -35,7 +35,7 @@ router.post('/', async (req, res) => {
   // INSERT INTO `user` (`userID`, `userName`, `userPwd`, `userPhone`, `userMail`, `userBirthday`, `userAddress`, `userImg`, `created_at`, `updated_at`) VALUES (NULL, '1', '1', '1', '1', '1', '1', '1', current_timestamp(), current_timestamp());
 
   // ("INSERT INTO `forumabout` (`ForumId`, `TitleMusic`, `TitleId`, `Memo`, `userID`) VALUES (NULL, ?, ?, ?, ?);",[TitleMusic,TitleId,Memo,userID])
-  const news1 =await query("INSERT INTO `forumabout` (`ForumId`, `TitleMusic`, `TitleId`, `Memo`, `userID`) VALUES (NULL, ?, ?, ?, ?);",[ForumTitle,ForumAction,ForumActionAns,userID])
+  const news1 =await query("INSERT INTO `forumabout` (`ForumId`, `TitleMusic`, `TitleId`, `Memo`, `userID`) VALUES (NULL, ?, ?, NULL, '3');",[ForumTitle,ForumAction])
   
   //  console.log(news.typeof)
   res.json(news1);
--- React/src/components/ForumComponents/ForumCard/ForumCard.js
+++ React/src/components/ForumComponents/ForumCard/ForumCard.js
@@ -24,7 +24,7 @@ class ForumCard extends Component {
   state = {
     boxShow: false,
     boxShowBtn: false,
-    userId: '',
+    userID: '',
     news: [
       {
         ForumId: '',
@@ -34,7 +34,7 @@ class ForumCard extends Component {
         NewsDate: '',
         ForumTitle:'',
         ForumAction:'',
-        ForumActionAns:'',
+        // ForumActionAns:'',
       },
     ],
    
@@ -49,17 +49,17 @@ class ForumCard extends Component {
     }
     if (getUserInfo()) {
       let user = getUserInfo()
-      // console.log(user[0].userName)
+      console.log(user[0].userID)
 
       if (user[0].userID) {
-        this.setState({ userId: user[0].userID })
+        this.setState({ userID: user[0].userID })
         this.setState({ username: user[0].userName })
         // console.log(this.state.username)
       } else {
-        this.setState({ userId: '' })
+        this.setState({ userID: '' })
       }
     } else {
-      this.setState({ userId: '' })
+      this.setState({ userID: '' })
     }
   }
   componentDidUpdate(e) {
@@ -88,7 +88,7 @@ class ForumCard extends Component {
     //input裡面的資料抓取功能
     localStorage.getItem("user") 
     const UserName = this.state.username
-    // UserName.value = this.state.news.UserId
+    // UserName.value = this.state.news.userID
     this.setState({ userName:UserName })
     console.log(this.state)
   }
@@ -114,12 +114,13 @@ handleForumAction = (event)=>{
   this.setState({ForumAction:event.target.value})
   console.log(event.target.value)
 }
-handleForumActionAns = (event)=>{
-  this.setState({ForumActionAns:event.target.value})
-  console.log(event.target.value)
-}
+// handleForumActionAns = (event)=>{
+//   this.setState({ForumActionAns:event.target.value})
+//   console.log(event.target.value)
+// }
 //送出之後把它存進資料庫
 handleSubmit = (event) => {
+  
   //sql語法
   // SELECT ForumAbout.userID,user.userName,ForumAbout.TitleMusic,ForumAbout.TitleId,ForumAbout.Memo FROM ForumAbout left JOIN user ON ForumAbout.userID = user.userID
 
@@ -142,11 +143,10 @@ handleSubmit = (event) => {
 let {
   ForumTitle,
   ForumAction,
-  ForumActionAns,
-  userId,
+  // ForumActionAns,
   userName}=this.state
 // console.log(this.state.ForumTitle)
-// console.log(this.state.userId)
+// console.log(this.state.userID)
 
 
   fetch('http://localhost:3030/forum/123', {
@@ -154,9 +154,6 @@ let {
                   body: JSON.stringify({
                  ForumTitle,
                  ForumAction,
-                 ForumActionAns,
-                 userId,
-                 userName
 
                   }), 
                   headers: new Headers({
@@ -180,13 +177,14 @@ let {
   this.setState({ boxShow: false })
   this.setState({ ForumTitle: this.state.ForumTitle })
   this.setState({ ForumAction: this.state.ForumAction })
-  this.setState({ ForumActionAns: this.state.ForumActionAns })
+  // this.setState({ ForumActionAns: this.state.ForumActionAns })
   
   alert('新增成功')
   console.log(this.state)
+  // this.location.reload()
 }
   render() {
-    const { boxShow, userId, boxShowBtn } = this.state
+    const { boxShow, boxShowBtn } = this.state
     //看回答的新增欄位
     const boxBtn = boxShowBtn ? (
       <div className="ForumCardDiv">
@@ -230,12 +228,12 @@ let {
             onChange={this.handleForumAction}
                />
           </div>
-          <textarea name="comment" className="ForumBtnAction"
+          {/* <textarea name="comment" className="ForumBtnAction"
             name="ForumActionAns" 
             value={this.state.ForumActionAns}          
             onChange={this.handleForumActionAns}
 
-              />
+              /> */}
           <input
             type="button"
             value="送出"
