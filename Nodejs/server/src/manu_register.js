1e82fe22c5ae6f557be01607ed51b5712dedc900
陳哲宇 <milk840118@yahoo.com.tw>
2020-06-30 18:03:25 +0800

    0630 廠商登入測試

---
+++ Nodejs/server/src/manu_register.js
@@ -0,0 +1,20 @@
+const express = require('express');
+// const db = require(__dirname + '/db_connect2');
+const router = express.Router();
+const query = require(__dirname + '/mysql');
+
+router.post('/', async (req, res) => {
+  let {userRegisterName, userRegisterEmail, userRegisterBir, userRegisterAddress, userRegisterMobile, userRegisterPwd} = req.body
+  let resData = { code: "", msg: ""}
+  await query("INSERT INTO `user` ( `userID`,`username`, `userPwd`, `userPhone`, `userMail`, `userBirthday`, `userAddress`, `userImg`) VALUES (NULL,?, ?, ?, ?, ?, ?, ?);", [userRegisterName, userRegisterPwd, userRegisterMobile, userRegisterEmail, userRegisterBir, userRegisterAddress,"userImg000.jpg"])
+
+  // const output = await query(`SELECT * FROM user WHERE userMail = ?`, [userRegisterEmail]);
+
+  resData = { code: 0, msg: "正確"}
+
+  res.json(resData);
+
+});
+
+module.exports = router;
+
