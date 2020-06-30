ba9e559bcd88dfd3c918ccc86e17659b27a5a2f8
Mengting-Lin <s400110605@gmail.com>
2020-06-30 18:02:38 +0800

    0630-cart-串接發票資訊及模組化

--- React/src/components/cart/cart-checkout.js
+++ React/src/components/cart/cart-checkout.js
@@ -1,12 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import CartDeliverInfo from './cart-deliverInfo.js';
 import CartItem from './cart-cartItem.js';
 
 function CartCheckout (props) {
 
-  const { cart, setCart, user, setUser, number, setNumber, totalPrice, checkstate, setcheckstate, checkcallback, ReceivingName, setReceivingName, ReceivingAddress, setReceivingAddress, ReceivingPhone, setReceivingPhone,  ReceivingEmail, setReceivingEmail } = props.allProps;  
+  const { 
+    cart, 
+    setCart, 
+    user, 
+    setUser, 
+    number, 
+    setNumber, 
+    totalPrice, 
+    checkstate, 
+    setcheckstate,
+    checkcallback,
+    ReceivingName, 
+    setReceivingName, 
+    ReceivingAddress, 
+    setReceivingAddress, 
+    ReceivingPhone, 
+    setReceivingPhone,  
+    ReceivingEmail, 
+    setReceivingEmail,    
+    radiostate, 
+    setRadiostate, 
+    radiocallback,
+    invoiceInfo,
+    setInvoiceInfo,
+   } = props.allProps;  
   
   // const [number, setNumber] = useState(cart[0].cartNumber)
   // let stock = cart[0].PQty;   
@@ -33,6 +56,7 @@ function CartCheckout (props) {
   // let arr =[2,1,3]
   // const [testArr,setTestArr]=useState(arr)
 
+  // minusCartNumber函式：點擊btn減少該商品之購物車數量
   const minusCartNumber = (e) =>{
     if (number == 0) {
       setNumber(0)
@@ -48,6 +72,7 @@ function CartCheckout (props) {
     }
   }
 
+  // plusCartNumber函式：點擊btn增加該商品之購物車數量
   const plusCartNumber = (e) =>{
     // let cart = cart[0]
     // console.log(cart[0].cartNumber)
@@ -132,18 +157,32 @@ function CartCheckout (props) {
                   <h2>統一發票</h2>
                   <fieldset>
                     <div>
-                      <label><input type="radio" name="invoice" id=""/> 會員載具</label>
+                      <label>
+                        <input type="radio" name="invoice" id="invoice1" value="會員載具" 
+                          onClick={(e)=>{ radiocallback(e.target) }}
+                        /> 會員載具
+                      </label>
                       <span>中獎時我們會將發票寄送至您的收件地址</span>
                     </div>
                     <div>
-                      <label><input type="radio" name="invoice" id=""/> 手機條碼載具</label>
+                      <label>
+                        <input type="radio" name="invoice" id="invoice2" value="手機條碼載具" 
+                          onClick={(e)=>{ radiocallback(e.target) }}
+                        /> 手機條碼載具
+                      </label>
                       <div className="cart-input1">
-                        <label htmlFor="einvoice">載具號碼</label>
-                        <input id="einvoice" type="text"/>
+                        <label htmlFor="invoice">載具號碼</label>
+                        <input id="invoicev" type="text" value={invoiceInfo}
+                          onChange={ (e)=>{ setInvoiceInfo(e.target.value) }}
+                        />
                       </div>
                     </div>
                     <div style={{padding:'0 0 20px 0'}}>
-                      <label><input type="radio" name="invoice" id=""/> 捐贈發票</label>
+                      <label>
+                        <input type="radio" name="invoice" id="invoice3" value="捐贈發票" 
+                          onClick={(e)=>{ radiocallback(e.target) }}
+                        /> 捐贈發票
+                      </label>
                       <span>提醒您，捐贈發票後無法變更成開立或索取紙本發票。</span>
                       <select>
                         <option value="台灣流浪兔保護協會">台灣流浪兔保護協會</option>
@@ -152,15 +191,23 @@ function CartCheckout (props) {
                       </select>
                     </div>
                     <div>
-                      <label><input type="radio" name="invoice" id=""/> 公司戶</label>
+                      <label>
+                        <input type="radio" name="invoice" id="invoice4" value="公司戶發票" 
+                          onClick={(e)=>{ radiocallback(e.target) }}
+                        /> 公司戶
+                      </label>
                       <span>提醒您，公司戶發票一旦開立，不得任意更改或改為個人戶發票。</span>
                       <div className="cart-input1">
-                        <label htmlFor="einvoice">統一編號</label>
-                        <input className="" id="einvoice" type="text"/>
+                        <label htmlFor="invoice">統一編號</label>
+                        <input className="companyinvoice" id="" type="text" value={invoiceInfo}
+                          onChange={ (e)=>{ setInvoiceInfo(e.target.value) }}
+                        />
                       </div>
                       <div className="cart-input1">
-                        <label htmlFor="einvoice">發票抬頭</label>
-                        <input className="" id="einvoice" type="text"/>
+                        <label htmlFor="invoice">發票抬頭</label>
+                        <input className="companyinvoice" id="" type="text" value={invoiceInfo}
+                          onChange={ (e)=>{ setInvoiceInfo(e.target.value) }}
+                        />
                       </div>
                     </div>
                 </fieldset>
--- React/src/container/cart-app.js
+++ React/src/container/cart-app.js
@@ -9,20 +9,16 @@ import CartPay from '../components/cart/cart-pay';
 import CartEnd from '../components/cart/cart-end';
 
 function CartApp (props) {
-   
-  // { cartItemId: 1, img: "商品圖片", product: "商品名稱", price: 商品單價, number: 商品數量 }
-
-  // coupon
   
+  // 會員資料
+  // [{"userID":3,"userName":"謝凱成","userPwd":"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3","userPhone":"0919390003","userMail":"003@gmail.com","userBirthday":"1912-09-22","userAddress":"桃園市龜山區公西里013鄰文三一街５０之１號","userImg":"userimg000","created_at":"2019-11-27 13:18:46","updated_at":"2019-11-25 13:18:48"}]
   const [user, setUser] = useState([])
-  // { userID: "", username: "", userAddress: "", userMail: "", userPhone: "" }
-  const [product, setProduct] = useState([])
-  // { PCategoryId:"", PId: "", PImg: "", PVideo: "", PNname: "", PPrice: "", PQty: "" }
-  // const [cartList, setCartList] = useState([])
-  // { cartItem: "", userID: "", pruductCategory: "", productId: "", cartNumber: "" }
+
+  // 購物車資料
+  // [{"PId":22,"PCategoryId":"樂器","PName":"KAWAI  K-30(SNW)直立式鋼琴","PImg":"Product_20200423164441.jpg","PPrice":60000,"PQty":1,"PIntro":"KAWAI  K-30(SNW)直立式鋼琴","Pdesciption":"採用日本原裝打擊系統、音搥、琴弦，外觀歐洲宮庭型","PInstrumentId":"鋼琴","PCompanyId":"F044","PClick":null,"created_at":"2020-08-16T06:18:00.000Z","update_at":"2020-08-17T06:18:00.000Z","PIId":null,"num":1}, 
+  // {"PId":22,"PCategoryId":"樂器","PName":"KAWAI  K-30(SNW)直立式鋼琴","PImg":"Product_20200423164441.jpg","PPrice":60000,"PQty":1,"PIntro":"KAWAI  K-30(SNW)直立式鋼琴","Pdesciption":"採用日本原裝打擊系統、音搥、琴弦，外觀歐洲宮庭型","PInstrumentId":"鋼琴","PCompanyId":"F044","PClick":null,"created_at":"2020-08-16T06:18:00.000Z","update_at":"2020-08-17T06:18:00.000Z","PIId":null,"num":1}]
   const [cart, setCart] = useState([])
-  // [{"cartItem":1,"memberId":"Lemon","productCategory":"課程","productId":1,"cartNumber":1,"PName":"【七月】鋼琴 - 平日班","PImg":"Product_20200423164331.jpg","PPrice":100, "PQty":2}, {"cartItem":2,"memberId":"L:","productCategory":"課程","productId":2,"cartNumber":1,"PName":"【七月】鋼琴 - 假日班","PImg":"Product_20200423164331.jpg","PPrice":100, "PQty":2}]
-  const [number, setNumber] = useState()
+  // const [number, setNumber] = useState()
   // let stock = 0; 
   // // console.log(stock);  
   // const coupon = 1500;
@@ -31,19 +27,81 @@ function CartApp (props) {
   // const orderPrice = totalPrice - coupon;
 
 
-  // 根據checkbox勾選狀態切換收件人資訊畫面
+  // checkbox勾選狀態，預設為不勾選
   const [ checkstate, setcheckstate ] = useState(false);
+  // checkcallback函式：點擊時切換checkbox勾選狀態
   const checkcallback = (e) =>{
     // console.log(e.checked)
     setcheckstate(e.checked)
   }
+  // 根據checkbox勾選狀態切換收件人資訊畫面
   useEffect(()=>{
     checkcallback(checkstate)
   },[])
   // console.log(checkstate);
 
 
-  // 收件人資訊欄位抓取會員自行填寫內容
+  // radio狀態，預設為不選取
+  const [ radiostate, setRadiostate ] = useState([false, false, false, false]);  
+  // 發票儲存方式，預設為空值
+  // const [ invoiceType, setInvoiceType ] = useState('');
+  // 發票資料，預設為空值
+  const [ invoiceInfo, setInvoiceInfo ] = useState('');  
+  // radiocallback函式：點擊時切換radio選取狀態並根據選項儲存發票資訊
+  const radiocallback = (e) =>{
+    
+    // console.log(document.getElementById("invoice2").checked)
+    let radiostate1 = document.getElementById("invoice1").checked;
+    let radiostate2 = document.getElementById("invoice2").checked;
+    let radiostate3 = document.getElementById("invoice3").checked;
+    let radiostate4 = document.getElementById("invoice4").checked;
+    setRadiostate([radiostate1, radiostate2, radiostate3, radiostate4])
+    // console.log(e.value);
+    let invoiceinfo2 = document.getElementById("invoicev").value;
+    var selectindex = document.querySelector("select").selectedIndex;
+    let invoiceinfo3 = document.querySelector("select").options[selectindex].value;
+    let invo4value1= document.getElementsByClassName("companyinvoice")[0].value;
+    let invo4value2= document.getElementsByClassName("companyinvoice")[1].value;
+    let invoiceinfo4 = [invo4value1, invo4value2];
+    // console.log(invoiceinfo4);
+    
+    // A == true ? A.value : (B == true ? B.value : (C == 1 ? C.value : (D == 1 ? D.value : '' )) )
+    setInvoiceInfo(
+      radiostate1 == true ? '' : (radiostate2 == true ? invoiceinfo2 : (radiostate3 == 1 ? invoiceinfo3 : (radiostate4 == 1 ? invoiceinfo4 : '' )))
+    )
+    // 切換radio選項時，先清空invoiceInfo
+    // if () {
+    //   setInvoiceInfo('')
+    // }
+  }
+  useEffect(()=>{
+    setInvoiceInfo('')
+    radiocallback(invoiceInfo)
+  },[])
+  // console.log(radiostate)
+  // console.log(radiostate[0])
+  // console.log(invoiceInfo);
+  
+
+  // const radiocallback = (e) =>{
+  //   // console.log(e);
+  //   // console.log(e.checked)
+  //   // console.log(e.value); 
+  //   setRadiostate(e.checked)
+  //   if (radiostate == 1) {   
+  //     // alert(e.value)
+  //     setInvoiceType(e.value) 
+  //   }
+  // }
+  // 根據radio狀態切換發票資訊畫面
+  // useEffect(()=>{
+  //   radiocallback(invoiceType)
+  // },[])
+  // console.log(radiostate)
+  // console.log(invoiceType)
+
+
+  // 收件人資訊欄位內容，預設為空值
   const [ ReceivingName, setReceivingName ] = useState('');
   const [ ReceivingAddress, setReceivingAddress ] = useState('');
   const [ ReceivingPhone, setReceivingPhone ] = useState('');
@@ -59,7 +117,15 @@ function CartApp (props) {
 // console.log('user:'+user[0]);
 
 
-// 從node路由獲取後端資料庫-商品資料
+// 從localStorage獲取-購物車資料
+useEffect(()=>{
+  const cartData = JSON.parse(localStorage.getItem('cart')); 
+  setCart(cartData) 
+},[])  
+// console.log('cart:'+cart);
+
+
+// 從路由獲取node後端資料庫-商品資料
 // useEffect(()=>{
 //   // sql語法：SELECT * FROM `product` WHERE 1
 //   fetch('http://localhost:3030/cart/2', { method: 'GET' })
@@ -72,7 +138,7 @@ function CartApp (props) {
 // console.log(product)
 
 
-// 從node路由獲取後端資料庫-購物車資料
+// 從路由獲取node後端資料庫-購物車資料
 // { "cartItemId": "1", "userID": "3", "productCategory": "課程", "productId": "1", "cartNumber": "1" }
 // useEffect(()=>{
 //   // sql語法：SELECT * FROM `cart` WHERE 1
@@ -83,14 +149,6 @@ function CartApp (props) {
 //     setCart(res)
 //   })
 // },[])
- 
-
-// 從localStorage獲取-購物車資料
-useEffect(()=>{
-  const cartData = JSON.parse(localStorage.getItem('cart')); 
-  setCart(cartData) 
-},[])  
-// console.log('cart:'+cart);
 
 
     return (
@@ -105,19 +163,15 @@ useEffect(()=>{
                     allProps={{
                         cart,
                         setCart,
-                        // number,
-                        // setNumber,
                         user,
                         setUser,
-                        // stock,
-                        // coupon,
-                        // totalPrice,
-                        // orderPrice,
                         checkstate,
+                        radiostate,
                         ReceivingName, 
                         ReceivingAddress, 
                         ReceivingPhone, 
                         ReceivingEmail, 
+                        invoiceInfo,
                     }}
                   />
                 </Route>
@@ -129,20 +183,13 @@ useEffect(()=>{
                 </Route>
                 <Route path="/cart">
                   <CartCheckout
-                    title={"購物車結帳"}
                     allProps={{
                         cart,
                         setCart,
-                        // number,
-                        // setNumber,
                         user,
                         setUser,
-                        // stock,
-                        // coupon,
-                        // itemPrice,
-                        // totalPrice,
-                        // orderPrice,
                         checkstate,
+                        setcheckstate,
                         checkcallback,
                         ReceivingName, 
                         setReceivingName, 
@@ -151,7 +198,12 @@ useEffect(()=>{
                         ReceivingPhone, 
                         setReceivingPhone, 
                         ReceivingEmail, 
-                        setReceivingEmail
+                        setReceivingEmail,
+                        radiostate, 
+                        setRadiostate,
+                        radiocallback,
+                        invoiceInfo,
+                        setInvoiceInfo,
                     }}
                   />
                 </Route>
