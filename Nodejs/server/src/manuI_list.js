b7314a082913a17dce758fefbec9e91b4924f516
Xavier0923 <64683412+Xavier0923@users.noreply.github.com>
2020-07-01 14:23:08 +0800

    07/01 除錯

--- Nodejs/server/src/manuI_list.js
+++ Nodejs/server/src/manuI_list.js
@@ -5,25 +5,26 @@ const upload = require(__dirname + "/upload-module");
 const query = require(__dirname + "/mysql");
 const sha1 = require("sha1");
 
-router.post('/',async (req,res) => {
-    let {
-        Pid,
-        PcategoryId,
-        Pimg,
-        Pname,
-        PInstrumentId,
-        PQty,
-        PIntro,
-        Pdesciption,
-        PCompanyId,
-        PPrice,
-        update_at,
-    } = req.body;
-    let resData = { code:'',msg :''};
+// router.post('/',async (req,res) => {
+//     let {
+//         Pid,
+//         PcategoryId,
+//         Pimg,
+//         Pname,
+//         PInstrumentId,
+//         PQty,
+//         PIntro,
+//         Pdesciption,
+//         PCompanyId,
+//         PPrice,
+//         update_at,
+//     } = req.body;
+//     let resData = { code:'',msg :''};
 
-    const [listdata] = await query(`SELECT * FROM product_instruments WHERE PId = 1`)
+//     const [listdata] = await query(`SELECT * FROM product_instruments WHERE PId = 1`)
 
-    console.log(listdata)
-    resData = { code:0, msg:'正確', data : listdata};
-    res.json(listdata);
-})
+//     console.log(listdata)
+//     resData = { code:0, msg:'正確', data : listdata};
+//     res.json(listdata);
+// })
+
